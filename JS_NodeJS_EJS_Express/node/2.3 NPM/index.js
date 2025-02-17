57e0182fb393137d5a90a82dfcea16c9792ba0f3
Kriszitan <xkiki2001@yahoo.com>
2025-02-17 09:03:58 +0200

    ...

--- JS_NodeJS_EJS_Express/node/2.3 NPM/index.js
+++
@@ -1,9 +0,0 @@
-
-//ESM
-import stpdName from "sillyname";
-import {randomSuperhero} from 'superheroes';
-
-for(let i = 0; i<10; i++){
-    console.log( `${stpdName()} alias ${randomSuperhero()}`);
-}
-
