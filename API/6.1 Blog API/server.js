3e91dace3852f78a771231dcc60e9abeed7b51cf
Károlyi Krisztián <145534392+krisztiankarolyi@users.noreply.github.com>
2025-03-18 13:05:56 +0200

    Update server.js
    Api url corrected

--- API/6.1 Blog API/server.js
+++ API/6.1 Blog API/server.js
@@ -6,7 +6,7 @@ import dotenv from "dotenv";
 dotenv.config();
 const app = express();
 const port = 3000;
-export const API_URL = "udemy-fullstack-web-projects-wkmw.onrender.com";
+export const API_URL = "https://udemy-fullstack-web-projects-wkmw.onrender.com";
 app.set('trust proxy', true);
 
 app.use(express.static("public"));
